37a2d5fb8a4eb2d03063fd2c2f136103de700b65
SONGHANSHI <973305062@qq.com>
2020-10-31 22:15:40 +0800

    trval

---
+++ app/Views/home.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+	<head>
+		<title>DGPT4711 Assignment1</title>
+	</head>
+	<body>
+		<h1>Detective Comics</h1>
+		<p><a href="/travel">Show the Heros</a>.</p>
+		<p><a href="/places">Show the Heros raw data</a></p>
+	</body>
+</html>
---
+++ app/Views/oneplace.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+	<head>
+		<title>DGPT4711 Assignment1</title>
+	</head>
+	<body>
+		<h1>Hero details</h1>
+		<p><strong>ID:</strong> {id}</p>
+		<p><strong>Name:</strong> {name}</p>
+                <p><strong>Sex:</strong> {sex}</p>
+                <p><strong>Birthday:</strong> {birthday}</p>
+                <p><strong>Height:</strong> {height}</p>
+                <p><strong>Weight:</strong> {weight}</p>
+                <p><strong>Ability:</strong> {ability}</p>
+		<p><strong>Description:</strong> {description}</p>
+		<p><img src="/image/{image}"/></p>
+		<p><a href="/home">Home</a></p>
+	</body>
+</html>
